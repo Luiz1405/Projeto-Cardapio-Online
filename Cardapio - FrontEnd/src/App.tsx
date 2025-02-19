6c60661cf8ad4d4170751a97f5dd78ef11fb2ae8
Luiz <kubaszewski001@hotmail.com>
2025-02-19 17:43:41 -0300

    Iniciando projeto do FrontEnd

---
+++ Cardapio - FrontEnd/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { FoodData } from './Interface/FoodData';
+import { Card } from './components/cards/card';
+
+function App() {
+  const data: FoodData[] = [];
+
+  return (
+    <div className="container">
+      <h1>Cardárpio</h1>
+      <div className="card-grid">
+        {data.map(foodData => 
+        <Card 
+        price={foodData.price} 
+        title={foodData.title} 
+        image={foodData.image}
+        />
+      )}
+    </div>
+  </div>
+  )
+}
+
+export default App
